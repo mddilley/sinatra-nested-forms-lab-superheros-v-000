dba9a1077bf6d43c509d7331a45fe04d90891f92
Mike Dilley <mike.dilley@gmail.com>
2018-12-17 16:37:56 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,16 @@
-class Team 
+class Team
 
-  
+  @@all = []
+
+  attr_accessor :name, :motto
+
+  def initialize(:name = nil, :motto = nil)
+    @name = name
+    @motto = motto
+  end
+
+  def self.all
+    @all
+  end
+
 end
